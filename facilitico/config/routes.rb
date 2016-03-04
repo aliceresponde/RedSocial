06a9ca8d780d62227955e5a05e5d3b811fd1845f
Alicia Beltran <aliceresponde@gmail.com>
2016-03-03 22:30:17 -0500

    add gema devise compatible con rails5

--- facilitico/config/routes.rb
+++ facilitico/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # get 'main/home'
-  # se crea la ruta para  main/home, por default rails busca la vista con el nombre del controlador 
+  # se crea la ruta para  main/home, por default cuando solo se pone el dominio rails busca la vista con el nombre del controlador 
+  # buscando la vista de la carpeta del controlador, asociado a la accion, asi este no tenga el render
   root 'main#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

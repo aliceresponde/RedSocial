222e32674635a138caef35010f0dea271a55c390
Alicia Beltran <aliceresponde@gmail.com>
2016-03-07 21:11:13 -0500

    create a link for destroy authencicated user

--- facilitico/app/controllers/users/omniauth_callbacks_controller.rb
+++ facilitico/app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,19 @@ class Users::OmniauthCallbacksController < ApplicationController
 		# raise params.to_yaml
 
 		# show parameters  from FaceBook that  we need
-		raise request.env["omniauth.auth"].to_yaml
+		# iu_id
+		# provider
+		# email - puede que no se tenga en todos los casos
+		#  las operaciones a DB deben ir en el modelo no en el controlador
+		# raise request.env["omniauth.auth"].to_yaml
+
+		@user = User.from_omniauth(request.env["omniauth.auth"])
+		# verifico si el usuario esta almacenado en DB
+		if @user.persisted?
+			# guarda la sesion 
+			@user.remember_me = true 
+			# autenticar al user  y redireccionarlo al home
+			sign_in_and_redirect @user , event: :authentication			
+		end
 	end
 end
--- facilitico/app/models/user.rb
+++ facilitico/app/models/user.rb
@@ -5,4 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
+     # Metodo de clase, que 
+     # Buscar si hay user con mis credenciales proveedor ui_id
+	def self.from_omniauth(auth)
+		# busco el usuario  con ese uid, asociado al provider
+		# si encuenta el usuario, retorna le primero q encuentre, si no
+		# simplemente lo crea con esos parametros
+		where(provider: auth[:provider] , uid: auth[:uid]).first_or_create do |user|
+			# ese user se ha creado
+			# veifio los datos q tengo de la info del user, como email, pass 
+			if auth[:info] #si tra el  hash del info, lleno los datos
+				user.email = auth[:info][:email]
+				user.name  = auth[:info][:name]
+			end
+			# creo una cadena, con caracteres aleatoreos (20) para emular el pass
+			# Nota   hay formas de  hacer  q email y pass no sea requerido pero como
+			# se puede via email, es mejor dejarlos
+			user.password = Devise.friendly_token[0,20]
+
+		end				
+			
+	end	
+
+	#Meotod de instancia  primero creo el objeto, y luego llamo el metodo 
 end
